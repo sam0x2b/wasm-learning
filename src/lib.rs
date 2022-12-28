b9578aa2d466831ee4463e69836c3ac9586025a2
yosef.ashley.2018 <hazelfelt@gmail.com>
2022-12-28 13:38:35 -0600

    Commenting + images with a side of renaming

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
+use web_sys::HtmlImageElement;
 use web_sys::{WebGlProgram, WebGl2RenderingContext, WebGlShader};
 
 #[wasm_bindgen]
@@ -10,109 +11,136 @@ pub fn init_panic_hook() {
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
     init_panic_hook();
+
+    // DOM
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.get_element_by_id("canvas").unwrap();
     let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
 
-    let context = canvas
+    let gl = canvas
         .get_context("webgl2")?
         .unwrap()
         .dyn_into()?;
 
+    // Shader Program
     let vert_shader = compile_shader(
-        &context,
+        &gl,
         WebGl2RenderingContext::VERTEX_SHADER,
         include_str!("./shader.vert"),
     )?;
     let frag_shader = compile_shader(
-        &context,
+        &gl,
         WebGl2RenderingContext::FRAGMENT_SHADER,
         include_str!("./shader.frag"),
     )?;
-    let program = link_program(&context, &vert_shader, &frag_shader)?;
-    context.use_program(Some(&program));
+    let program = link_program(&gl, &vert_shader, &frag_shader)?;
+    gl.use_program(Some(&program));
 
-    let vertices: [f32; 9] = [-0.7, -0.7, 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0];
+    // GL VAOs and VBOs
+    const VERTEX_COUNT: usize = 3;
+    {
+        // Position data
+        let positions: [f32; VERTEX_COUNT*3] = [
+            -0.7, -0.7, 0.0,
+             0.7, -0.7, 0.0,
+             0.0,  0.7, 0.0
+        ];
 
-    let vert_buffer = context.create_buffer().ok_or("failed to create buffer")?;
-    context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&vert_buffer));
+        let buffer = gl.create_buffer().ok_or("failed to create buffer")?;
+        gl.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&buffer));
 
-    // Note that `Float32Array::view` is somewhat dangerous (hence the
-    // `unsafe`!). This is creating a raw view into our module's
-    // `WebAssembly.Memory` buffer, but if we allocate more pages for ourself
-    // (aka do a memory allocation in Rust) it'll cause the buffer to change,
-    // causing the `Float32Array` to be invalid.
-    //
-    // As a result, after `Float32Array::view` we have to be very careful not to
-    // do any memory allocations before it's dropped.
-    unsafe {
-        let vert_array = js_sys::Float32Array::view(&vertices);
-        context.buffer_data_with_array_buffer_view(
-            WebGl2RenderingContext::ARRAY_BUFFER,
-            &vert_array,
-            WebGl2RenderingContext::STATIC_DRAW,
-        );
+        // spooky memory danger (see Float32Array::view documentation)
+        unsafe {
+            let array = js_sys::Float32Array::view(&positions);
+            gl.buffer_data_with_array_buffer_view(
+                WebGl2RenderingContext::ARRAY_BUFFER,
+                &array,
+                WebGl2RenderingContext::STATIC_DRAW,
+            );
+        }
+
+        gl.vertex_attrib_pointer_with_i32(0, 3, WebGl2RenderingContext::FLOAT, false, 0, 0);
+        gl.enable_vertex_attrib_array(0);
     }
 
-    context.vertex_attrib_pointer_with_i32(0, 3, WebGl2RenderingContext::FLOAT, false, 0, 0);
-    context.enable_vertex_attrib_array(0);
+    {
+        // Texture data
+        let image = HtmlImageElement::new().unwrap();
+        image.set_src("texture.png");
+        // but... we have to make sure the image is downloaded before handing it to WebGL
+        // which probably involves async stuff but idk how to do that cleanly just yet
 
-    context.clear_color(0.0, 0.0, 0.0, 1.0);
-    context.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
+        gl.tex_image_2d_with_u32_and_u32_and_html_image_element(
+            WebGl2RenderingContext::TEXTURE_2D,
+            0,
+            WebGl2RenderingContext::RGBA.try_into().unwrap(), // internal format
+            WebGl2RenderingContext::RGBA, // format
+            WebGl2RenderingContext::UNSIGNED_BYTE, // type
+            &image
+        ).unwrap();
 
-    context.draw_arrays(
+        // let texture_coords: [f32; VERTEX_COUNT*2] = [
+
+        // ];
+    }
+
+    // Drawing
+    gl.clear_color(0.0, 0.0, 0.0, 1.0);
+    gl.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
+
+    gl.draw_arrays(
         WebGl2RenderingContext::TRIANGLES,
         0,
-        (vertices.len() / 3) as i32,
+        VERTEX_COUNT as i32,
     );
     Ok(())
 }
 
 pub fn compile_shader(
-    context: &WebGl2RenderingContext,
+    gl: &WebGl2RenderingContext,
     shader_type: u32,
     source: &str,
 ) -> Result<WebGlShader, String> {
-    let shader = context
+    let shader = gl
         .create_shader(shader_type)
         .ok_or_else(|| String::from("Unable to create shader object"))?;
-    context.shader_source(&shader, source);
-    context.compile_shader(&shader);
+    gl.shader_source(&shader, source);
+    gl.compile_shader(&shader);
 
-    if context
+    if gl
         .get_shader_parameter(&shader, WebGl2RenderingContext::COMPILE_STATUS)
         .as_bool()
         .unwrap_or(false)
     {
         Ok(shader)
     } else {
-        Err(context
+        Err(gl
             .get_shader_info_log(&shader)
             .unwrap_or_else(|| String::from("Unknown error creating shader")))
     }
 }
 
 pub fn link_program(
-    context: &WebGl2RenderingContext,
+    gl: &WebGl2RenderingContext,
     vert_shader: &WebGlShader,
     frag_shader: &WebGlShader,
 ) -> Result<WebGlProgram, String> {
-    let program = context
+    let program = gl
         .create_program()
         .ok_or_else(|| String::from("Unable to create shader object"))?;
 
-    context.attach_shader(&program, vert_shader);
-    context.attach_shader(&program, frag_shader);
-    context.link_program(&program);
+    gl.attach_shader(&program, vert_shader);
+    gl.attach_shader(&program, frag_shader);
+    gl.link_program(&program);
 
-    if context
+    if gl
         .get_program_parameter(&program, WebGl2RenderingContext::LINK_STATUS)
         .as_bool()
         .unwrap_or(false)
     {
         Ok(program)
     } else {
-        Err(context
+        Err(gl
             .get_program_info_log(&program)
             .unwrap_or_else(|| String::from("Unknown error creating program object")))
     }
