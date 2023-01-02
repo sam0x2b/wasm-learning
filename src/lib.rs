1e0215a06ee963e50320b853aa0b2f01116769d9
sam0x2b <keycattie@ya.ru>
2023-01-02 08:11:59 +0300

    broke it

--- src/lib.rs
+++ src/lib.rs
@@ -23,10 +23,6 @@ pub fn start() -> Result<(), JsValue> {
     let gl = canvas.get_context("webgl2")?.unwrap().dyn_into::<GL>()?;
     let gl = Rc::new(gl);
 
-    // texture loading
-    // todo texture ids
-    texture_from_image(gl.clone(), "./test.webp");
-
     // Shader Program
     let vert_shader = compile_shader(gl.clone(), GL::VERTEX_SHADER, include_str!("./shader.vert"))?;
     let frag_shader = compile_shader(
@@ -36,23 +32,89 @@ pub fn start() -> Result<(), JsValue> {
     )?;
     let program = link_program(gl.clone(), &vert_shader, &frag_shader)?;
     gl.use_program(Some(&program));
+    let program = Rc::new(program);
 
-    // GL VAOs and VBOs
-    const VERTEX_COUNT: usize = 3;
+    // Texture Setup
+    // (referring to https://webgl2fundamentals.org/webgl/lessons/webgl-image-processing.html)
+    // let image = document
+    //     .get_element_by_id("texture")
+    //     .unwrap()
+    //     .dyn_into::<web_sys::HtmlImageElement>()?;
+
+    // (create and bind texture)
+    texture_from_image(gl.clone(), "./test.webp", program.clone());
+    // let texture = gl.create_texture().unwrap();
+    // gl.active_texture(GL::TEXTURE0); // fixme when we get to actually making stuff, we need to be sure we are using GL::TEXTUREx thing (coz compatibility)
+    // gl.bind_texture(GL::TEXTURE_2D, Some(&texture));
+
+    // // (image uniform location)
+    // let u_texture = gl.get_uniform_location(&program, "u_texture").unwrap();
+    // gl.uniform1i(Some(&u_texture), 0); // tell the shader to look at texture unit 0
+
+    // gl.pixel_storei(GL::UNPACK_FLIP_Y_WEBGL, 1);
+    // gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MIN_FILTER, GL::NEAREST as i32);
+    // gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MAG_FILTER, GL::NEAREST as i32);
+    // gl.tex_image_2d_with_u32_and_u32_and_html_image_element(
+    //     GL::TEXTURE_2D,
+    //     0,
+    //     GL::RGBA as i32,
+    //     GL::RGBA,
+    //     GL::UNSIGNED_BYTE,
+    //     &image,
+    // ).expect("Not a valid texture");
+
+    // VAOs and VBOs
+    const VERTEX_COUNT: usize = 6;
+    let vao = gl.create_vertex_array().unwrap();
+    gl.bind_vertex_array(Some(&vao));
+
+    // (attribute locations)
+    let a_position = gl.get_attrib_location(&program, "a_position") as u32;
+    let a_uv = gl.get_attrib_location(&program, "a_uv") as u32;
+
     {
-        // Position data
-        let positions: [f32; VERTEX_COUNT * 3] = [-0.7, -0.7, 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0];
-        let texture_uv: [f32; VERTEX_COUNT * 2] = [0., 0., 1., 0., 1., 1.];
+        const POSITION_SIZE: usize = 3; // size of position data per vertex
+        let positions: [f32; VERTEX_COUNT * POSITION_SIZE] = [
+            // (hypothetically we could use indexing to cut this down to just /four/ vertices
+            // worth of data... but right now i just dont feel like it)
+            -0.7, 0.7, 0.0, // top left
+            -0.7, -0.7, 0.0, // bottom left
+            0.7, -0.7, 0.0, // bottom right
+            -0.7, 0.7, 0.0, // top left
+            0.7, -0.7, 0.0, // bottom right
+            0.7, 0.7, 0.0, // top right
+        ]; // god i love alignment
 
         let buffer = gl.create_buffer().ok_or("failed to create buffer")?;
         gl.bind_buffer(GL::ARRAY_BUFFER, Some(&buffer));
-        // spooky memory danger (see Float32Array::view documentation)
         unsafe {
+            // spooky Float32Array::view memory danger
             let array = js_sys::Float32Array::view(&positions);
             gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &array, GL::STATIC_DRAW);
         }
-            gl.vertex_attrib_pointer_with_i32(0, 3, GL::FLOAT, false, 0, 0);
-            gl.enable_vertex_attrib_array(0);
+        gl.vertex_attrib_pointer_with_i32(a_position, POSITION_SIZE as i32, GL::FLOAT, false, 0, 0);
+        gl.enable_vertex_attrib_array(a_position);
+    }
+
+    {
+        const UV_SIZE: usize = 2; // size of uv coordinate data, per vertex.
+        let uvs: [f32; VERTEX_COUNT * UV_SIZE] = [
+            0.0, 1.0, // top left
+            0.0, 0.0, // bottom left
+            1.0, 0.0, // bottom right
+            0.0, 1.0, // top left
+            1.0, 0.0, // bottom right
+            1.0, 1.0, // top right
+        ];
+
+        let buffer = gl.create_buffer().ok_or("failed to create buffer")?;
+        gl.bind_buffer(GL::ARRAY_BUFFER, Some(&buffer));
+        unsafe {
+            let array = js_sys::Float32Array::view(&uvs);
+            gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &array, GL::STATIC_DRAW);
+        }
+        gl.vertex_attrib_pointer_with_i32(a_uv, UV_SIZE as i32, GL::FLOAT, false, 0, 0);
+        gl.enable_vertex_attrib_array(a_uv);
     }
 
     // Drawing
@@ -109,15 +171,20 @@ pub fn link_program(
     }
 }
 
-pub fn texture_from_image(gl: Rc<GL>, url: &str) {
+pub fn texture_from_image(gl: Rc<GL>, url: &str, program: Rc<WebGlProgram>) {
     let image = Rc::new(RefCell::new(HtmlImageElement::new().unwrap()));
     let image_for_move = Rc::clone(&image);
 
     let onload = Closure::wrap(Box::new(move || {
         let texture = gl.create_texture();
-
         gl.active_texture(GL::TEXTURE0); // fixme when we get to actually making stuff, we need to be sure we are using GL::TEXTUREx thing (coz compatibility)
         gl.bind_texture(GL::TEXTURE_2D, texture.as_ref());
+        // (image uniform location)
+        let u_texture = gl
+            .get_uniform_location(program.clone().as_ref(), "u_texture")
+            .unwrap();
+        gl.uniform1i(Some(&u_texture), 0); // tell the shader to look at texture unit 0
+
         gl.pixel_storei(GL::UNPACK_FLIP_Y_WEBGL, 1);
         gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MIN_FILTER, GL::NEAREST as i32);
         gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MAG_FILTER, GL::NEAREST as i32);
@@ -131,7 +198,7 @@ pub fn texture_from_image(gl: Rc<GL>, url: &str) {
             GL::UNSIGNED_BYTE,
             &image_for_move.borrow(), // spooky business
         )
-        .expect("Not a valid texture");
+        .expect("Valid texture");
     }) as Box<dyn Fn()>);
 
     let image = image.borrow_mut();
